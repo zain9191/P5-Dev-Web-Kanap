9e1818247fe8bdc158f9f65ec87e82c02920d67e
your_username <zain.frayha@gmail.com>
2023-05-13 16:45:38 +0200

    item are shown in the cart, need to add delete btn

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,27 +1,82 @@
-// const selectedId = localStorage.getItem('productId');
-// const productColor = localStorage.getItem('selectedColor');
-// const productTitle = localStorage.getItem('title');
-// const productPrice = localStorage.getItem('price');
-// const productCount = localStorage.getItem('count');
+let storedCart = localStorage.getItem('cart');
+console.log(storedCart)
 
-
-// // Now you can use the stored data
-// console.log('Selected ID:', selectedId);
-// console.log('Product color:', productColor);
-// console.log('Selected title:', productTitle);
-// console.log('Product price:', productPrice);
-// console.log('product Count:', productCount);
-
-
-
-// Get the cart data from LocalStorage:
-let storedCart = localStorage.getItem('order');
-
-// Parse it back into an array:
+let cart;
 if (storedCart) {
-  cart = JSON.parse(storedCart);
-  console.log(cart)
+    cart = JSON.parse(storedCart);
 } else {
-  cart = [];
-  alert("data was't imported")
+    cart = [];
 }
+
+const cartAndFormContainer = document.querySelector("#cartAndFormContainer");
+
+let cartSection = document.createElement('section');
+cartSection.classList.add('cart');
+cartAndFormContainer.appendChild(cartSection);
+
+let totalPrice = 0;
+let totalQuantity = 0;
+
+cart.forEach((product) => {
+    let productArticle = document.createElement('article');
+    productArticle.classList.add('cart__item');
+    productArticle.dataset.id = product.id;
+    productArticle.dataset.color = product.color;
+
+    let productImgDiv = document.createElement('div');
+    productImgDiv.classList.add('cart__item__img');
+    let productImg = document.createElement('img');
+    productImg.src = product.imgURL;
+    productImg.alt = product.title;
+    productImgDiv.appendChild(productImg);
+
+    let productContentDiv = document.createElement('div');
+    productContentDiv.classList.add('cart__item__content');
+
+    let productDescriptionDiv = document.createElement('div');
+    productDescriptionDiv.classList.add('cart__item__content__description');
+    let productTitle = document.createElement('h2');
+    productTitle.textContent = product.title;
+    let productColor = document.createElement('p');
+    productColor.textContent = product.color;
+    let productPrice = document.createElement('p');
+    productPrice.textContent = `${product.price} €`;
+    productDescriptionDiv.append(productTitle, productColor, productPrice);
+
+    let productSettingsDiv = document.createElement('div');
+    productSettingsDiv.classList.add('cart__item__content__settings');
+    let productQuantityDiv = document.createElement('div');
+    productQuantityDiv.classList.add('cart__item__content__settings__quantity');
+    let quantityText = document.createElement('p');
+    quantityText.textContent = `Qté : `;
+    let quantityInput = document.createElement('input');
+    quantityInput.type = "number";
+    quantityInput.name = "itemQuantity";
+    quantityInput.min = "1";
+    quantityInput.max = "100";
+    quantityInput.value = product.count;
+    quantityInput.classList.add('itemQuantity');
+    productQuantityDiv.append(quantityText, quantityInput);
+    let deleteDiv = document.createElement('div');
+    deleteDiv.classList.add('cart__item__content__settings__delete');
+    let deleteText = document.createElement('p');
+    deleteText.textContent = "Supprimer";
+    deleteText.classList.add('deleteItem');
+    deleteDiv.appendChild(deleteText);
+    productSettingsDiv.append(productQuantityDiv, deleteDiv);
+
+    productContentDiv.append(productDescriptionDiv, productSettingsDiv);
+
+    productArticle.append(productImgDiv, productContentDiv);
+
+    cartSection.appendChild(productArticle);
+    
+    totalPrice += parseFloat(product.price) * product.count;
+    totalQuantity += product.count;
+});
+
+const totalQuantityElement = document.querySelector("#totalQuantity");
+const totalPriceElement = document.querySelector("#totalPrice");
+
+totalQuantityElement.textContent = totalQuantity;
+totalPriceElement.textContent = totalPrice.toFixed(2);  // toFixed(2) will ensure that the price is always shown with two decimal places
--- front/js/product.js
+++ front/js/product.js
@@ -1,141 +1,77 @@
 function getQueryParams() {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const id = urlParams.get("id");
-    return { id };
-  }
-  
-  
-  async function getProductDetails(productId) {
-    const url = `http://localhost:3000/api/products/${productId}`;
-    const response = await fetch(url);
-    if (!response.ok) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const id = urlParams.get("id");
+  return { id };
+}
+
+async function getProductDetails(productId) {
+  const url = `http://localhost:3000/api/products/${productId}`;
+  const response = await fetch(url);
+  if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const product = await response.json();
-    // console.log(product)
-    return product;
   }
+  const product = await response.json();
+  return product;
+}
+
+(async () => {
+  const queryParams = getQueryParams();
+  const productId = queryParams.id;
+  const product = await getProductDetails(productId);
+
+  const productImageContainer = document.querySelector(".item__img");
+  const img = document.createElement("img");
+  img.src = product.imageUrl;
+  img.alt = product.description;
+  productImageContainer.appendChild(img);
+
+  const productTitle = document.querySelector("#title");
+  const name = product.name;
+  productTitle.innerHTML = name;
+
+  const productPrice = document.querySelector("#price");
+  const price = product.price;
+  productPrice.innerHTML = price;
+
+  const productDescription = document.querySelector("#description");
+  const description = product.description;
+  productDescription.innerHTML = description;
+
+  let colorDropdown = document.querySelector('#colors');
+  let colors = product.colors; 
   
-
-
-  (async () => {
-    const queryParams = getQueryParams();
-  
-    const productId = queryParams.id;
-  
-    const product = await getProductDetails(productId);
-
-    const productImageContainer = document.querySelector(".item__img");
-  
-    const img = document.createElement("img");
-    img.src = product.imageUrl;
-    img.alt = product.description;
-    productImageContainer.appendChild(img);
-
-
-    const productTitle = document.querySelector("#title");
-    const name = product.name;
-    productTitle.innerHTML = name;
-
-
-    const productPrice = document.querySelector("#price");
-    const price = product.price;
-    productPrice.innerHTML = price;
-
-
-    const productDescription = document.querySelector("#description");
-    const description = product.description;
-    productDescription.innerHTML = description;
-
-    // const price = document.createElement("p");
-    // price.innerText = product.description;
-    // productTitle.appendChild(price)
-
-
-
-    let colorDropdown = document.querySelector('#colors');
-    let colors = product.colors; 
-    
-    colors.forEach(color => {
+  colors.forEach(color => {
       let option = document.createElement('option');
       option.value = color;
       option.innerText = color;
       colorDropdown.appendChild(option);
-    });
-    
-    
+  });
 
-    const addToCart = document.querySelector("#addToCart");
-    // console.log(addToCart)
-
-    
-    addToCart.addEventListener("click",(event)=>{
-        event.preventDefault();
-        // get product detalies and add them local storage
-
-        // id
-        const queryParams = getQueryParams();
-        const productId = queryParams.id;
-
-        alert(productId)
-
-
-        // color
-        const colorOptieions = document.querySelector('#colors');
-        const selectedColor = colorOptieions.value;
-        alert(selectedColor)
-
-
-        // name
-        const productTitle = document.querySelector("#title");
-        const selectedTitle = productTitle.value;
-        alert(product.name)
-
-        // price
-        const productPrice = document.querySelector("#price");
-        const selectedProductPrice = productTitle.value;
-        alert(product.price)
-
-
-        // count
-        const productCount = document.querySelector("#quantity");
-        const selectedcount = productCount.value;
-        alert(selectedcount)
-
-
-
-        // Let's say you have an array of product objects that you want to store:
-        let order = [
-            {
-            id: productId,
-            color: selectedColor,
-            title: product.name,
-            price: product.price,
-            count: selectedcount,
-            },
-        ];
-        
-        localStorage.setItem('order', JSON.stringify(order));
-        
-        
-
-
-
-
-        // // Store the data
-        // localStorage.setItem('productId', productId);
-        // localStorage.setItem('selectedColor', selectedColor);
-        // localStorage.setItem('title', product.name);
-        // localStorage.setItem('price', price);
-        // localStorage.setItem('count', selectedcount);
-
-        // Redirect to the other page
-        window.location.href = 'cart.html';
-
-
-    })
-
-
-  })();
+  const addToCart = document.querySelector("#addToCart");
   
+  addToCart.addEventListener("click",(event)=>{
+      event.preventDefault();
+      
+      const colorOptieions = document.querySelector('#colors');
+      const selectedColor = colorOptieions.value;
+
+      const productCount = document.querySelector("#quantity");
+      const selectedcount = productCount.value;
+
+      let newProduct = {
+          imgURL : product.imageUrl,
+          id: productId,
+          color: selectedColor,
+          title: product.name,
+          price: product.price,
+          count: selectedcount,
+      };
+
+      let cart = JSON.parse(localStorage.getItem('cart')) || [];
+      cart.push(newProduct);
+      localStorage.setItem('cart', JSON.stringify(cart));
+
+      window.location.href = 'cart.html';
+  })
+})();
